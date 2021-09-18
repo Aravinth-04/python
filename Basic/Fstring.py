7180221342d88c46828297a32493a5d2b9e5eb15
Aravinth <74677913+Aravinth-04@users.noreply.github.com>
2021-09-18 19:10:56 +0530

    Create Fstring.py

---
+++ Basic/Fstring.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+remaning_age=int(age)
+
+remaning_years=90-remaning_age
+remaning_months=remaning_years*12;
+remaning_days=remaning_years*365;
+
+#fString
+result=f"your remaning years {remaning_years}, your remaning months, {remaning_months} your remaning day  {remaning_days}"
+print(result)
+
